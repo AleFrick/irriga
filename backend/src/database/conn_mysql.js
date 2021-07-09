42089431d7c0e4331bc5062a9ee4b89a21e66b73
Alex Schmidt <alex.schmidt@avmb.com.br>
2021-07-09 15:52:23 -0300

    Criada a conexão com o banco de dados e testes iniciais para consumir a api

---
+++ backend/src/database/conn_mysql.js
@@ -0,0 +1,11 @@
+require('dotenv/config');
+
+const Sequelize =  require('sequelize')
+const sequelize = new Sequelize(process.env.NAME_DB, process.env.USER_DB, process.env.PASS_DB, {
+  host: process.env.HOST_DB,
+  dialect: 'mysql',
+  port: process.env.PORT_DB  
+})
+
+
+module.exports = { sequelize }
---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const cors = require('cors');
+const port = 4851
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+app.use(routes)
+
+app.listen(port, ()=>{
+  console.log(`\n Servidor rodando na porta ${port} `)
+});
