2908f0a32b3aba24148b88337cbb4f5cc80cef6a
Alex Schmidt <alex.schmidt@avmb.com.br>
2021-07-09 16:46:06 -0300

    ajuste

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ const routes = express.Router()
 const rotaClima = require('../src/routes/Clima')
 const modelCities = require('../src/models/Cities')
 
- 
+
 routes.use(rotaClima)
 
 module.exports = routes;
