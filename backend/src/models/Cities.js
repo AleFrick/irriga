4c26b9330fb568a3346b1c3cbb0ab3522a17d7a2
Alex Schmidt <alex.schmidt@avmb.com.br>
2021-07-09 16:42:52 -0300

    ajuste conexão e  model para tabela cities

---
+++ backend/src/models/Cities.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize')
+var moment = require('moment');
+const conn = require('../database/conn_mysql')
+
+const Cities = conn.sequelize.define('cities', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },  
+    name: {
+      type: Sequelize.STRING
+    },
+    hr_altered:{
+      type: Sequelize.TIME
+    },
+    dt_altered: {
+      type: Sequelize.DATEONLY,    
+      get: function() {
+        return moment(this.getDataValue('DateTime')).format('DD/MM/YYYY')
+      }
+    },    
+    dt_created: {
+      type: Sequelize.DATEONLY,    
+      get: function() {
+        return moment(this.getDataValue('DateTime')).format('DD/MM/YYYY')
+      } 
+    }
+  }, {  
+    tableName: 'cities',
+    timestamps: false,
+  })
+
+  Cities.beforeCreate(async(Cities) => {        
+    Cities.dt_criacao = moment().format('YYYY-MM-DD')
+    Cities.dt_alteracao = moment().format('YYYY-MM-DD')
+    Cities.hr_alteracao = moment().format('HH:mm')
+  })
+  Cities.beforeUpdate(async(Cities) => {    
+    Cities.dt_alteracao = moment().format('YYYY-MM-DD')
+    Cities.hr_alteracao = moment().format('HH:mm')
+  })
+
+  Cities.sync({force: false, alter: true}).then(() => {
+    console.log('Table Cities loaded successfuly');
+  });
+  
+  module.exports = Cities;
+
